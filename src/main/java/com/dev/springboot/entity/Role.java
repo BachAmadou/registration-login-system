9140c36a9dbd13ad746e4c89a2090788cc3e6f4b
Bachir <bafosth@yahoo.fr>
2024-03-04 09:58:19 -0500

    Initial commit

---
+++ src/main/java/com/dev/springboot/entity/Role.java
@@ -0,0 +1,28 @@
+package com.dev.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    // To allow many to many binding with User class
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+}
---
+++ src/main/java/com/dev/springboot/entity/User.java
@@ -0,0 +1,41 @@
+package com.dev.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // To allow many to many binding with Role Class
+    // FetchType.EAGER when we load User Jpa entity, hibernate will also load its associated roles
+    // CascadeType.ALL whatever action performed on User entity can also be applicable to Role entity. because User is a parent and Role is a child
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Role> roles = new ArrayList<>();
+}
